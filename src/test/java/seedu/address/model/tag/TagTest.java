5eff96d0df7f103420ab638f45d15642d6028aff
Chee Hin <tancheeehin@gmail.com>
2025-10-11 17:59:03 +0800

    style: Reorder import statements

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,9 +1,9 @@
 package seedu.address.model.tag;
 
-import static seedu.address.testutil.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
