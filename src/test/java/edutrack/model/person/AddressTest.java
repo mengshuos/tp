2b0744b8f91d57458dcfad666036fd6dc649b06d
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-10-15 20:37:51 +0800

    Add assertion for empty string to be valid address

--- src/test/java/edutrack/model/person/AddressTest.java
+++ src/test/java/edutrack/model/person/AddressTest.java
@@ -21,13 +21,13 @@ public class AddressTest {
         assertTrue(Address.isValidAddress(null));
 
         // invalid addresses
-        assertFalse(Address.isValidAddress("")); // empty string
         assertFalse(Address.isValidAddress(" ")); // spaces only
 
         // valid addresses
         assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
         assertTrue(Address.isValidAddress("-")); // one character
         assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
+        assertTrue(Address.isValidAddress("")); // empty string valid for optional address
     }
 
     @Test
