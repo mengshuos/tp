827075cda9762fdf437c1c14173cb273c32b9c08
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-10-15 20:16:01 +0800

    Add square parentheses in AddCommand MESSAGE_USAGE for ADDRESS parameter

--- src/main/java/edutrack/logic/commands/AddCommand.java
+++ src/main/java/edutrack/logic/commands/AddCommand.java
@@ -26,7 +26,7 @@ public class AddCommand extends Command {
             + PREFIX_NAME + "NAME "
             + "[" + PREFIX_PHONE + "PHONE" + "] "
             + "[" + PREFIX_EMAIL + "EMAIL" + "] "
-            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_ADDRESS + "ADDRESS" + "] "
             + "[" + PREFIX_TAG + "TAG]... "
             + "[" + PREFIX_TAG + "GROUP]...\n"
             + "Example: " + COMMAND_WORD + " "
