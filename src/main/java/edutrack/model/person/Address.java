fa32cfbc726b5f298645fde8d31b64739dd24bb2
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-10-15 21:26:49 +0800

    Change INVALID_ADDRESS_DESC to one that is actually invalid under optional constraint

--- src/main/java/edutrack/model/person/Address.java
+++ src/main/java/edutrack/model/person/Address.java
@@ -14,7 +14,7 @@ public class Address {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = "[^\\s\\p{Cntrl}][\\p{Print}\\p{Space}]*";
 
     public final String value;
 
