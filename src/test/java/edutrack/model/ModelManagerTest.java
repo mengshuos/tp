ce43f1379be48558d9be31400ea6681ac635888b
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-10-16 17:23:04 +0800

    refactor: Update tests involving NameContainsKeywordsPredicate

--- src/test/java/edutrack/model/ModelManagerTest.java
+++ src/test/java/edutrack/model/ModelManagerTest.java
@@ -118,8 +118,7 @@ public class ModelManagerTest {
 
         // different filteredList -> returns false
         String[] keywords = ALICE.getName().fullName.split("\\s+");
-        modelManager.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(keywords),
-                NameContainsKeywordsPredicate.Mode.NAME));
+        modelManager.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
         assertFalse(modelManager.equals(new ModelManager(addressBook, userPrefs)));
 
         // resets modelManager to initial state for upcoming tests
