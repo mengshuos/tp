e3ae3e5f29e45d220298b0d3dce75a0be3bf3af8
Chee Hin <tancheeehin@gmail.com>
2025-10-16 22:04:02 +0800

    style: Fix declaration order

--- src/main/java/edutrack/commons/core/Messages.java
+++ src/main/java/edutrack/commons/core/Messages.java
@@ -6,9 +6,9 @@ package edutrack.commons.core;
  */
 public final class Messages {
 
-    private Messages() {} // prevent instantiation
-
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+
+    private Messages() {}
 
 }
