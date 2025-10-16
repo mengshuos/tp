5cb62fee39a0ef16bf6af7b9ccef981f7f789d8b
Chee Hin <tancheeehin@gmail.com>
2025-10-16 17:36:50 +0800

    test: Add testing for findtag feature

---
+++ src/test/java/edutrack/logic/parser/FindTagCommandParserTest.java
@@ -0,0 +1,90 @@
+package edutrack.logic.parser;
+
+import static edutrack.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static edutrack.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static edutrack.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import edutrack.logic.commands.FindTagCommand;
+import edutrack.model.tag.Tag;
+
+public class FindTagCommandParserTest {
+
+    private FindTagCommandParser parser = new FindTagCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingPrefix_throwsParseException() {
+        // no prefix provided
+        assertParseFailure(parser, "friends",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidTagName_throwsParseException() {
+        // invalid tag with special characters
+        assertParseFailure(parser, " t/friend*",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagCommand.MESSAGE_USAGE));
+
+        // invalid tag with spaces
+        assertParseFailure(parser, " t/needs help",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_preamblePresent_throwsParseException() {
+        // preamble before the prefix
+        assertParseFailure(parser, "some text t/friends",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTagCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindTagCommand() throws Exception {
+        // valid tag without whitespace
+        Tag expectedTag = new Tag("friends");
+        FindTagCommand expectedCommand = new FindTagCommand(expectedTag);
+        assertParseSuccess(parser, " t/friends", expectedCommand);
+
+        // valid tag with leading/trailing whitespaces
+        assertParseSuccess(parser, " t/  friends  ", expectedCommand);
+    }
+
+    @Test
+    public void parse_validTagWithUnderscore_returnsFindTagCommand() throws Exception {
+        // valid tag with underscore
+        Tag expectedTag = new Tag("needs_help");
+        FindTagCommand expectedCommand = new FindTagCommand(expectedTag);
+        assertParseSuccess(parser, " t/needs_help", expectedCommand);
+    }
+
+    @Test
+    public void parse_validTagWithHyphen_returnsFindTagCommand() throws Exception {
+        // valid tag with hyphen
+        Tag expectedTag = new Tag("high-priority");
+        FindTagCommand expectedCommand = new FindTagCommand(expectedTag);
+        assertParseSuccess(parser, " t/high-priority", expectedCommand);
+    }
+
+    @Test
+    public void parse_validTagWithSlash_returnsFindTagCommand() throws Exception {
+        // valid tag with slash
+        Tag expectedTag = new Tag("CS2103T/W13");
+        FindTagCommand expectedCommand = new FindTagCommand(expectedTag);
+        assertParseSuccess(parser, " t/CS2103T/W13", expectedCommand);
+    }
+
+    @Test
+    public void parse_caseInsensitiveTag_returnsFindTagCommand() throws Exception {
+        // tags are case-insensitive
+        Tag expectedTag = new Tag("Friends");
+        FindTagCommand expectedCommand = new FindTagCommand(expectedTag);
+        assertParseSuccess(parser, " t/Friends", expectedCommand);
+    }
+}
+
